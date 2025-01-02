61a6e5e49b29cb5af1afc0591e6dbab2adb3214d
Herdeir0 <david.herdeiro@gmail.com>
2025-01-02 17:23:27 +0000

    Refactored the filter

---
+++ js/catalog-filter.js
@@ -0,0 +1,19 @@
+function filterElements(selectedClass) {
+    // Get all elements inside the .items container
+    const allItems = document.querySelectorAll("catalog a");
+  
+    // Loop through each item
+    allItems.forEach(item => {
+      if (selectedClass === 'all') {
+        // Show all items if 'all' is selected
+        item.classList.add("show");
+      } else {
+        // Check if the item contains the selected class
+        if (item.classList.contains(selectedClass)) {
+          item.classList.add("show");
+        } else {
+          item.classList.remove("show");
+        }
+      }
+    });
+}
