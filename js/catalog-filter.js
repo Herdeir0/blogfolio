0bd27ac8be5612dcf82b1f207debb8d7f5e8baad
Herdeir0 <david.herdeiro@gmail.com>
2025-01-21 16:16:31 +0000

    Major Updates

--- js/catalog-filter.js
+++ js/catalog-filter.js
@@ -1,20 +1,38 @@
-function filterElements(selectedClass) {
+function filterElementsArticle(selectedClass) {
     // Get all elements inside the .items container
-    const allItems = document.querySelectorAll("catalog a");
+    const allItems = document.querySelectorAll("catalog.article a");
   
     // Loop through each item
     allItems.forEach(item => {
-      if (selectedClass === 'all') {
+      if (selectedClass === 'article-all') {
         // Show all items if 'all' is selected
-        item.classList.add("show");
+        item.classList.add("a-show");
       } else {
         // Check if the item contains the selected class
         if (item.classList.contains(selectedClass)) {
-          item.classList.add("show");
+          item.classList.add("a-show");
         } else {
-          item.classList.remove("show");
+          item.classList.remove("a-show");
         }
       }
     });
+}
 
+function filterElementsProject(selectedClass) {
+  // Get all elements inside the .items container
+  const allItems = document.querySelectorAll("catalog.project a");
+
+  // Loop through each item
+  allItems.forEach(item => {
+    if (selectedClass === 'project-all') {
+      // Show all items if 'all' is selected
+      item.classList.add("p-show");
+    } else {
+      // Check if the item contains the selected class
+      if (item.classList.contains(selectedClass)) {
+        item.classList.add("p-show");
+      } else {
+        item.classList.remove("p-show");
+      }
+    }});
 }
